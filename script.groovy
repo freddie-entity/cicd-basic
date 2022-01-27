4a8727e75cfc96b6e21d0874a2ab7fad385251d2
[freddie-entity] <nguyentrungtinc4nh1516@gmail.com>
2022-01-27 20:36:39 +0700

    credentials added

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: 'DOCKER_CREDENTIALS', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh 'docker build -t freddieentity/java-maven:1.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh 'docker push freddieentity/java-maven:1.0'
@@ -15,9 +15,5 @@ def buildImage() {
 def deployApp() {
     echo 'deploying the application...'
 } 
-
-def skipDeploy() {
-    echo 'skipping deploy stage...'
-}
 
 return this
